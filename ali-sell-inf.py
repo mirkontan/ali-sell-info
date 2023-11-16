3a63d34cc1617224ddd818223ab7ead41eb71b8e
mirkontan <148462539+mirkontan@users.noreply.github.com>
2023-11-16 13:20:12 +0100

    Update ali-sell-inf.py

--- ali-sell-inf.py
+++ ali-sell-inf.py
@@ -134,8 +134,8 @@ if uploaded_images:
             extracted_data_per_image_aliexpress = pd.DataFrame([data])
             extracted_data_per_image_aliexpress['PLATFORM'] = 'ALIEXPRESS'
             extracted_data_per_image_aliexpress['FILENAME'] = uploaded_image.name
-            # st.write(extracted_data_per_image_aliexpress)
-            # st.write(data)        
+            st.write(extracted_data_per_image_aliexpress)
+            st.write(data)        
             # Create a DataFrame for the extracted data of this image
             ## df_extraction = pd.DataFrame(extracted_data_per_image)
             # Create a DataFrame for the extracted data of this image
